ff6ec508937aeb9ce62c692827d0d79fe0fb48c3
zan-afn <zanfn@example.com>
2026-07-30 10:59:09 +0700

    refactor: remove unused import

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Enums\CreditAmount;
 use App\Enums\UserRole;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 
--- tests/Feature/AvailabilityTest.php
+++ tests/Feature/AvailabilityTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use App\Enums\CreditAmount;
-use App\Models\AvailabilityRequest;
 use App\Models\Kost;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
