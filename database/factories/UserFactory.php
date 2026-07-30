6b0152cbcef3c49ea2d9b3c20507923ffde86d42
zan-afn <zanfn@example.com>
2026-07-30 10:50:46 +0700

    fix: update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,44 +2,52 @@
 
 namespace Database\Factories;
 
+use App\Enums\CreditAmount;
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
+
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+
+            'password' => static::$password ??= Hash::make('password123'),
+
+            'role' => UserRole::REGULAR,
+
+            'credit' => CreditAmount::REGULAR->value,
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function owner(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state(fn() => [
+            'role' => UserRole::OWNER,
+            'credit' => CreditAmount::OWNER->value,
+        ]);
+    }
+
+    public function regular(): static
+    {
+        return $this->state(fn() => [
+            'role' => UserRole::REGULAR,
+            'credit' => CreditAmount::REGULAR->value,
+        ]);
+    }
+
+    public function premium(): static
+    {
+        return $this->state(fn() => [
+            'role' => UserRole::PREMIUM,
+            'credit' => CreditAmount::PREMIUM->value,
         ]);
     }
 }
